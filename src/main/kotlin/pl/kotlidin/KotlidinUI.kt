9ae3f82e87ad395ada42d55a724e7a9245bfe7af
MadMax <madmax1028@gmail.com>
2016-07-06 18:21:58 +0200

    added person form in modal window

--- src/main/kotlin/pl/kotlidin/KotlidinUI.kt
+++ src/main/kotlin/pl/kotlidin/KotlidinUI.kt
@@ -2,11 +2,11 @@ package pl.kotlidin
 
 import com.vaadin.annotations.Push
 import com.vaadin.annotations.Theme
+import com.vaadin.data.fieldgroup.BeanFieldGroup
 import com.vaadin.data.util.BeanItemContainer
 import com.vaadin.server.VaadinRequest
 import com.vaadin.spring.annotation.SpringUI
-import com.vaadin.ui.Button
-import com.vaadin.ui.UI
+import com.vaadin.ui.*
 import com.vaadin.ui.themes.ValoTheme
 import org.springframework.beans.factory.annotation.Autowired
 
@@ -30,18 +30,27 @@ class KotlidinUI @Autowired constructor(private val personRepository: PersonRepo
 			isSpacing = true
 			
 			this += Button("Create person", { event ->
-				personRepository.save(Person("ggg", "hhh"))
-				refreshRows()
+				showPersonForm(Person(), {
+					personRepository.save(it)
+					refreshRows()
+				})
 			})
 			
 			this += table("Persons", persons) {
 				setSizeFull()
 				addGeneratedColumn("", { table, itemId, columnId -> horizontalLayout() {
 					isSpacing = true
+					this += Button("Edit", { event ->
+						showPersonForm(itemId as Person, {
+							personRepository.save(it)
+							refreshRows()
+						})
+					})
 					this += Button("Copy", { event ->
-						val person = personRepository.findOne((itemId as Person).id)
-						personRepository.save(person.copy(lastName = person.id.toString()))
-						refreshRows()
+						showPersonForm((itemId as Person).copy(), {
+							personRepository.save(it)
+							refreshRows()
+						})
 					})
 					this += Button("Delete", { event ->
 						personRepository.delete(itemId as Person)
@@ -51,5 +60,43 @@ class KotlidinUI @Autowired constructor(private val personRepository: PersonRepo
 				setVisibleColumns("id", "firstName", "lastName", "")
 			} withExpandRatio 1F
 		}
+	}
+	
+	private class PersonForm : FormLayout() {
+		val firstName = TextField("First name")
+		val lastName = TextField("Last name")
+		val saveButton = Button("Save")
+		val cancelButton = Button("Cancel")
+		
+		init {
+			firstName.nullRepresentation = ""
+			lastName.nullRepresentation = ""
+			
+			this += firstName
+			this += lastName
+			this += horizontalLayout(saveButton, cancelButton) {
+				isSpacing = true
+			}
+		}
+	}
+	
+	private fun showPersonForm(person: Person, onSubmit: (Person) -> Unit) {
+		val form = PersonForm()
+		val beanFieldGroup = BeanFieldGroup.bindFieldsBuffered(person, form)
+		val window = Window("Person form")
+		
+		form.setMargin(true)
+		form.saveButton.addClickListener {
+			beanFieldGroup.commit();
+			window.close();
+			onSubmit(person);
+		}
+		form.cancelButton.addClickListener { window.close(); }
+		
+		window.content = form
+		window.isModal = true
+		window.isResizable = false
+		window.center()
+		addWindow(window)
 	}
 }
