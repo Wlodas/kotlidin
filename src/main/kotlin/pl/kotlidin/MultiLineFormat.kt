ed801891978c5ca9a0852024d3b3477bbfaf48b0
MadMax <madmax1028@gmail.com>
2016-07-15 16:15:36 +0200

    optimized formats

--- src/main/kotlin/pl/kotlidin/MultiLineFormat.kt
+++ src/main/kotlin/pl/kotlidin/MultiLineFormat.kt
@@ -3,8 +3,8 @@ package pl.kotlidin
 import com.p6spy.engine.spy.appender.MessageFormattingStrategy
 
 class MultiLineFormat : MessageFormattingStrategy {
-	override fun formatMessage(connectionId: Int, now: String, elapsed: Long, category: String, prepared: String, sql: String): String {
-		return when(sql.isNotEmpty()) {
+	override fun formatMessage(connectionId: Int, now: String?, elapsed: Long, category: String?, prepared: String?, sql: String?): String {
+		return when(sql!!.isNotEmpty()) {
 			true -> "took $elapsed ms | $category | connection $connectionId\n$sql"
 			else -> "took $elapsed ms | $category | connection $connectionId"
 		}
--- src/main/kotlin/pl/kotlidin/PrettySqlFormat.kt
+++ src/main/kotlin/pl/kotlidin/PrettySqlFormat.kt
@@ -6,8 +6,8 @@ import org.hibernate.engine.jdbc.internal.BasicFormatterImpl
 class PrettySqlFormat : MessageFormattingStrategy {
 	private val formatter = BasicFormatterImpl()
 	
-	override fun formatMessage(connectionId: Int, now: String, elapsed: Long, category: String, prepared: String, sql: String): String {
-		return when(sql.isNotEmpty()) {
+	override fun formatMessage(connectionId: Int, now: String?, elapsed: Long, category: String?, prepared: String?, sql: String?): String {
+		return when(sql!!.isNotEmpty()) {
 			true -> "took $elapsed ms | $category | connection $connectionId${formatter.format(sql)}"
 			else -> "took $elapsed ms | $category | connection $connectionId"
 		}
